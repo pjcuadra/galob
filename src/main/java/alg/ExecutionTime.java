ac58a83985867343469c7ed1a1effa6723e9da78
Pedro Joaquin Cuadra Chamorro <pedro.cuadrachamorro001@stud.fh-dortmund.de>
2016-12-07 21:10:50 +0100

    new: dev: Implement helper functions for ExecutionTime class based on paper

--- src/main/java/alg/ExecutionTime.java
+++ src/main/java/alg/ExecutionTime.java
@@ -1,7 +1,7 @@
 /* 
  * This class should implement all the necessary methods to implement
  * Load Balancing Task Scheduling based on Multi-Population Genetic in
- * Cloud Computing (Wang Bei, LI Jun).. Note that this is not actually 
+ * Cloud Computing (Wang Bei, LI Jun). Note that this is not actually 
  * taking into account the load balancing we try to achieve but the final 
  * result will be built on top of this. Also we are limiting the 
  * implementation to a single population. 
@@ -13,11 +13,137 @@
 
 package alg;
 
-// For now this class is just a placeholder. Remove this comment once
-// class is implemented.
-
 public class ExecutionTime {
+	
+	/* 
+	 * Execution Time Cost matrix with ETC[i][j] where i is the
+	 * node and j is the task index. 
+	 */ 
+	private double[][] ETC;
+	
+	// The ETC is need from the beginning of time
+	public ExecutionTime(double[][] ETC)
+	{
+		this.ETC = ETC;
+	}
+	
+	/**
+	 * Calculate the CONV matrix from a Chromosome
+	 * 
+	 * According to "Load Balancing Task Scheduling based on 
+	 * Multi-Population Genetic in Cloud Computing" (Wang Bei, 
+	 * LI Jun) 
+	 * 
+	 * @param chromosome allocation nodes array indexed by 
+	 * 					 task index
+	 * 					  
+	 * @return CONV matrix
+	 */
+	private double[][] createCONVMatrix(int[] chromosome)
+	{
+		double[][] CONV = new double[ETC.length][ETC[0].length];
+		int currTask = 0;
+		
+		// Just set to one where it is allocated
+		for (currTask = 0; currTask < chromosome.length; currTask++)
+		{
+			CONV[chromosome[currTask]][currTask] = 1;
+		}
+		
+		return CONV;
+	}
 
-	// TODO: Add all necessary functions for execution time optimization
+	
+	/**
+	 * Get the execution time of every node given a chromosome
+	 * 
+	 * According to "Load Balancing Task Scheduling based on 
+	 * Multi-Population Genetic in Cloud Computing" (Wang Bei, 
+	 * LI Jun), equation (1)
+	 * 
+	 * @param chromosome allocation nodes array indexed by 
+	 * 					 task index
+	 * @return the sum of execution times per node as an array
+	 *         indexed by node index 
+	 */
+	public double[] getSumTime(int[] chromosome)
+	{
+		double[][] CONV = createCONVMatrix(chromosome);
+		double[] sumTime = new double[CONV.length];
+		int i = 0, j = 0;
+		
+		// Iterate over the tasks
+		for (i = 0; i < CONV.length; i++)
+		{
+			sumTime[i] = 0;
+					
+			// Iterate over the nodes
+			for (j = 0; j < CONV[i].length; j++)
+			{
+				// Add all execution times
+				sumTime[i] += CONV[i][j]*ETC[i][j];
+			}
+		}		
+		
+		return sumTime;
+	}
+	
+	/**
+	 * Get total execution time given a Chromosome
+	 * 
+	 * According to "Load Balancing Task Scheduling based on 
+	 * Multi-Population Genetic in Cloud Computing" (Wang Bei, 
+	 * LI Jun), equation (2)
+	 * 
+	 * @param chromosome allocation nodes array indexed by 
+	 * 					 task index
+	 * 
+	 * @return total execution time of a given Chromosome
+	 */
+	public double getTotalTime(int[] chromosome)
+	{
+		double[] sumTime = getSumTime(chromosome);
+		double totalTime = 0;
+		
+		for (double time : sumTime)
+		{
+			totalTime += time;
+		}
+		
+		return totalTime;
+	}
+	
+	
+	/**
+	 * Get the load imbalance of given Chromosome
+	 * 
+	 * According to "Load Balancing Task Scheduling based on 
+	 * Multi-Population Genetic in Cloud Computing" (Wang Bei, 
+	 * LI Jun), equation (4)
+	 * 
+	 * @param chromosome allocation nodes array indexed by 
+	 * 					 task index
+	 * @return load imbalance of a Chromosome
+	 */
+	public double getLoad(int[] chromosome)
+	{
+		double[] sumTime = getSumTime(chromosome);
+		double load = 0;
+		double avgTime = getTotalTime(chromosome)/chromosome.length;
+		
+		// First calculate sum = (sumTime(i) - averageTime)^2
+		for (double time: sumTime)
+		{
+			load += Math.pow(time - avgTime, 2);
+		}
+		
+		// Now multiply the sum with 1/(M - 1)
+		load = load/((double)(sumTime.length -1));
+		
+		// And finally take the square root
+		load = Math.sqrt(load);
+		
+		return load;
+	}
 	
 }
--- src/test/java/alg/ExecutionTimeUT.java
+++ src/test/java/alg/ExecutionTimeUT.java
@@ -3,7 +3,9 @@
  */
 package alg;
 
-//import static org.junit.Assert.*;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -16,6 +18,16 @@ import org.junit.Test;
  *
  */
 public class ExecutionTimeUT {
+	
+	private Random randomGen;
+	
+	private double[][] ones;
+	private int[] chromosome;
+	private ExecutionTime executionTimeGA;
+	private int numTask;
+	private int executors;
+	final static int maxNumTask = 16 /* Actual max*/ - 1 /* 0 is not possible so add 1 after*/;
+	final static int maxNumExecutors = 16 /* Actual max*/  - 1 /* 0 is not possible so add 1 after*/;
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,6 +48,32 @@ public class ExecutionTimeUT {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		
+		randomGen = new Random();
+		
+		numTask =  1 + randomGen.nextInt(maxNumTask);
+		executors =  1 + randomGen.nextInt(maxNumExecutors);
+		
+		ones = new double[executors][numTask];
+		chromosome = new int[numTask];
+		
+		// Randomly initialize the chormosome allocating to a random node
+		for (int currTask = 0; currTask < chromosome.length; currTask++)
+		{
+			chromosome[currTask] =  randomGen.nextInt(executors);
+		}
+		
+		// Initialize ones matrix with ones}
+		for (int currRow = 0; currRow < ones.length; currRow++)
+		{
+			for (int currCol = 0; currCol < ones[0].length; currCol++)
+			{
+				ones[currRow][currCol] = 1;
+			}
+		}
+		
+		executionTimeGA =  new ExecutionTime(ones);
+		
 	}
 
 	/**
@@ -44,10 +82,63 @@ public class ExecutionTimeUT {
 	@After
 	public void tearDown() throws Exception {
 	}
+	
+	
+	/**
+	 * Get the amount of task allocated to a given node
+	 * @param node node index
+	 * @return amount of task allocated to given node
+	 */
+	private int getTaskAllocateOnNode(int node)
+	{
+		int amount = 0;
+		
+		for (int currGen: chromosome)
+		{
+			if (currGen == node)
+			{
+				amount++;
+			}
+		}
+		
+		return amount;
+		
+	}
 
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
+	public void getSumTime()
+	{
+		double[] sumTime;
+		
+		sumTime = executionTimeGA.getSumTime(chromosome);
+		
+		/* 
+		 * Since our ETC matrix has only ones the execution time per node
+		 * should be the amount of nodes assigned to it.
+		 */		
+		for (int currNodeTime = 0; currNodeTime < executors; currNodeTime++)
+		{
+			assertEquals(sumTime[currNodeTime], getTaskAllocateOnNode(currNodeTime), 0.01);
+		}
+
+	}
+	
+	@Test
+	public void calculateTotalTime(){
+		double totalTime;
+		
+		totalTime = executionTimeGA.getTotalTime(chromosome);
+		
+		/* 
+		 * Since our ETC matrix has only ones the total execution time 
+		 * should be the amount of tasks.
+		 */	
+		assertEquals(totalTime, numTask, 0.01);
+	}
+	
+	@Test
+	public void loadImabalance(){
+		executionTimeGA.getLoad(chromosome);
 	}
 
 }
