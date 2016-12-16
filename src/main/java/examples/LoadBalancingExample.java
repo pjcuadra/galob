5482317bfa15e2fd307310869ed3bddf5a3f5824
Pedro Cuadra <pedro.cuadrachamorro001@stud.fh-dortmund.de>
2016-12-16 13:22:01 +0100

    new: dev: Add load balancing example place holder

---
+++ src/main/java/examples/LoadBalancingExample.java
@@ -0,0 +1,23 @@
+package examples;
+
+/**
+ * Example of load balacing optimization.
+ *
+ * @author Pedro Cuadra
+ * @author Sudheera Bandi
+ *
+ */
+public class LoadBalancingExample {
+
+  /**
+   * Main function.
+   * @param args command line parameters
+   */
+  public static void main(String[] args) {
+    // TODO: This example is a place holder for an actuall example that will be
+    // developed later.
+    System.out.println("Dummy example");
+
+  }
+
+}
