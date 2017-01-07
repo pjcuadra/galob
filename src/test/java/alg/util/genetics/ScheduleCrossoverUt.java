d96de59b51d3c7ac1f30e285002b98ec06d5fb1f
sudheerareddy.bandi <sudheera.bandi001@stud.fh-dortmund.de>
2017-01-07 17:11:30 +0100

    Crossover function for the scheduler

---
+++ src/test/java/alg/util/genetics/ScheduleCrossoverUt.java
@@ -0,0 +1,118 @@
+/**
+ * Schedule Crossover unit testing.
+ */
+
+package alg.util.genetics;
+
+import static org.junit.Assert.assertEquals;
+
+import alg.util.Util;
+
+import org.jenetics.Chromosome;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * Schedule Mutator unit testing.
+ * 
+ * @author Pedro Cuadra
+ * @author Sudheera Bandi
+ *
+ */
+public class ScheduleCrossoverUt {
+
+  private Random randomGen;
+
+  private int numTask;
+  private int executors;
+  static final int maxNumTask = 9 /* Actual max*/;
+  static final int maxNumExecutors = 6 /* Actual max*/;
+  static final int maxPopulation = 50 /* Actual max*/;
+  private ScheduleCrossover crossover;
+  private double[][] delta;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  /**
+   * Unit testing set-up.
+   * 
+   * @throws Exception falure exception
+   */
+  @Before
+  public void setUp() throws Exception {
+
+    randomGen = new Random();
+
+    numTask =  1 + randomGen.nextInt(maxNumTask);
+    executors =  1 + randomGen.nextInt(maxNumExecutors);
+
+    // Create new dependencies randomly
+    delta = Util.getDeltaMatrix(numTask);
+
+    // Always crossover
+    crossover = new ScheduleCrossover(delta, 1);
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  private int countAltersOfChromosome(Chromosome<ScheduleGene> originalChromosome, 
+      Chromosome<ScheduleGene> childChromosome) {
+    int alterationsCount = 0;
+    ScheduleGene expected;
+    ScheduleGene actual;
+    // Count number of alterations
+    for (int locus = 0; locus < originalChromosome.toSeq().size(); locus++) {
+
+      expected =  originalChromosome.getGene(locus);
+      actual =  childChromosome.getGene(locus);
+
+      if (!expected.equals(actual)) {
+        alterationsCount++;
+      }
+
+    }
+
+    return alterationsCount;
+  }
+
+  @Test
+  public void singleCrossover() {
+    ScheduleChromosome parentChromosome1 = new ScheduleChromosome(delta, executors);
+    ScheduleChromosome parentChromosome2 = new ScheduleChromosome(delta, executors);
+    ScheduleChromosome childChromosome = parentChromosome1.clone();
+    int alterations = 0;
+    int alterationsCount = 0;
+
+    // Crossover the chromosome
+    childChromosome = (ScheduleChromosome)crossover
+        .crossoverChromosome(parentChromosome1, parentChromosome2);
+
+    if (childChromosome !=  null) {
+      alterations = 2;
+
+
+      alterationsCount = countAltersOfChromosome(parentChromosome1, childChromosome);
+    }
+
+    assertEquals(alterationsCount, alterations);
+    System.out.println(alterationsCount);
+    
+  }
+
+ 
+     
+}
