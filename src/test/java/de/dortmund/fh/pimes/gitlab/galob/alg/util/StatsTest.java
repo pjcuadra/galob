030bd7c51352428c92ff87d3f83af75ca502e9a5
Pedro Cuadra <pedro.cuadrachamorro001@stud.fh-dortmund.de>
2017-03-30 14:09:53 +0200

    new: dev: Implemente alg.Util package tests

--- src/test/java/de/dortmund/fh/pimes/gitlab/galob/alg/util/StatsTest.java
+++ src/test/java/de/dortmund/fh/pimes/gitlab/galob/alg/util/StatsTest.java
@@ -21,9 +21,17 @@
 
 package de.dortmund.fh.pimes.gitlab.galob.alg.util;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import de.dortmund.fh.pimes.gitlab.galob.alg.LoadBalancingFitnessCalculator;
+import de.dortmund.fh.pimes.gitlab.galob.alg.util.graph.GraphNode;
+import de.dortmund.fh.pimes.gitlab.galob.alg.util.jenetics.ScheduleAllele;
+import de.dortmund.fh.pimes.gitlab.galob.alg.util.jenetics.ScheduleChromosome;
+import de.dortmund.fh.pimes.gitlab.galob.alg.util.jenetics.ScheduleGene;
 
 //import java.util.Random;
 
@@ -36,14 +44,6 @@ import org.junit.Test;
  */
 public class StatsTest {
   /**
-   * Random generator.
-   */
-  // private Random randomGen;
-  /**
-   * Number of tasks.
-   */
-  // private int numTask;
-  /**
    * Max. number of tasks.
    */
   static final int MAX_NUM_TASKS = 16;
@@ -51,7 +51,27 @@ public class StatsTest {
    * Max. number cores.
    */
   static final int MAX_NUM_CORES = 16;
-
+  /**
+   * Heterogeneous Computing environment.
+   */
+  private HeterogeneousComputingEnv env;
+  /**
+   * Chromomsome.
+   */
+  private ScheduleChromosome chromosome;
+  /**
+   * Fitness Calculator.
+   */
+  private LoadBalancingFitnessCalculator fitnessCalc;
+  /**
+   * Stats factory.
+   */
+  private StatsFactory sf;
+  /**
+   * Delta for double comparison.
+   */
+  private static final double EPSILON = 0.00001;
+  
   /**
    * Test environment set-up.
    * 
@@ -60,31 +80,86 @@ public class StatsTest {
    */
   @Before
   public void setUp() throws Exception {
+    env = HeterogeneousComputingEnv.ofRandomUnitary(MAX_NUM_TASKS, 
+        MAX_NUM_CORES, 
+        true);
+    chromosome = new ScheduleChromosome(env);
+    fitnessCalc = new LoadBalancingFitnessCalculator(env, 0);
+    
+    // Create the factory
+    sf = new StatsFactory(env, fitnessCalc);
 
   }
 
   @Ignore("Note yet implemented")
   @Test
   public void testGetNodesExecutionTime() throws Exception {
-    throw new RuntimeException("not yet implemented");
+    int numCores = env.getNumberOfExecutors();
+    int numTasks = env.getNumberOfTasks();
+    double [] nodesExecTime = new double[numCores];
+    
+    
+    
+    
   }
-
-  @Ignore("Note yet implemented")
-  @Test
-  public void testGetActualFinishTime() throws Exception {
-    throw new RuntimeException("not yet implemented");
-  }
-
+  
   @Ignore("Note yet implemented")
   @Test
   public void testGetExecutionUnit() throws Exception {
     throw new RuntimeException("not yet implemented");
   }
 
-  @Ignore("Note yet implemented")
+  
+  /*
+   *  +-+           +-+
+   *  |0+----------->2|
+   *  +++           +++
+   *   |             ^
+   *   |    +-+      |
+   *   +---->1+------+
+   *        +-+
+   */
+  private HeterogeneousComputingEnv nestedDependency() throws Exception {
+    final int numTasks = 3;
+    final int numCores = 2;
+    HeterogeneousComputingEnv envTest = new HeterogeneousComputingEnv(numTasks, numCores);
+    GraphNode[] tasks = new GraphNode[numTasks];
+    
+    // Add all tasks
+    for (int i = 0; i < numTasks; i++) {
+      tasks[i] = envTest.addUnitExecutionTimeTask();
+    }
+    
+    // Create dependencies
+    envTest.addDependency(tasks[0], tasks[1], 1);
+    envTest.addDependency(tasks[0], tasks[2], 1);
+    envTest.addDependency(tasks[1], tasks[2], 1);  
+        
+    return envTest;
+  }
+
+ 
   @Test
   public void testGetTotalTime() throws Exception {
-    throw new RuntimeException("not yet implemented");
+    
+    // Test case 1
+    HeterogeneousComputingEnv envTest = nestedDependency();
+    
+    ScheduleChromosome chromosomeTest = new ScheduleChromosome(envTest);
+    
+    // Guarantee that task 0 and 2 are in the same core and 1 in the other
+    chromosomeTest.scheduleSeq.get(0).mutate(new ScheduleAllele(0, 0, 0, 0));
+    chromosomeTest.scheduleSeq.get(1).mutate(new ScheduleAllele(0, 0, 1, 1));
+    chromosomeTest.scheduleSeq.get(2).mutate(new ScheduleAllele(0, 0, 2, 0));
+    
+    for (ScheduleGene gene: chromosomeTest.scheduleSeq) {
+      System.out.println("Task ID: " + gene.getAllele().getTaskId());
+      System.out.println("Core ID: " + gene.getAllele().getExecutorId());
+    }
+    
+    Stats stat = sf.ofChromosome(chromosomeTest);
+    
+    assertEquals(5, stat.getTotalTime(), EPSILON);
   }
 
   @Ignore("Note yet implemented")
@@ -103,5 +178,14 @@ public class StatsTest {
   @Test
   public void testGetFitness() throws Exception {
     throw new RuntimeException("not yet implemented");
+  }
+
+  @Test
+  public void testGetChromosome() throws Exception {
+    // Create a stat instance
+    Stats stat = sf.ofChromosome(chromosome);
+    
+    // Check that the chromosome was added to the stats object
+    assertEquals(chromosome, stat.getChromosome());
   }
 }
